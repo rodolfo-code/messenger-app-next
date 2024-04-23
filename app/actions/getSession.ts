3f74200afcb4ed39055781f5b027f0d88faf1f1d
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-04-23 16:37:52 -0300

    fix auth route

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,8 +1,9 @@
 import nextAuth, { getServerSession } from "next-auth";
+import { authOptions } from "../libs/auth";
 
-import { authOptions } from "../api/auth/[...nextauth]/route";
-// import {...options} from "../api/auth/[...nextauth]/route"
+// import { authOptions } from "../api/auth/[...nextauth]/route";
+// import authOptions from "../api/auth/[...nextauth]/route"
 
 export default async function getSession() {
-  return await getServerSession();
+  return await getServerSession(authOptions);
 }
